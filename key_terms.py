98c21833627384c2bd84bbd62bfa4aeb3d39eecf
JB <y.v.barash@gmail.com>
2022-05-16 23:04:32 +0200

    Stage 3/4. POS-tagging added.

--- key_terms.py
+++ key_terms.py
@@ -11,6 +11,7 @@ lemmatizer = nltk.WordNetLemmatizer()
 for n in root[0]:
     print(f'{n[0].text}:')
     lemmas = []
+    posed = []
     tokenized = nltk.word_tokenize(n[1].text.lower())
     lemmatized = [lemmatizer.lemmatize(i) for i in tokenized]
     for l in lemmatized:
@@ -18,6 +19,15 @@ for n in root[0]:
             continue
         lemmas.append(lemmatizer.lemmatize(l))
 
-    c = Counter(lemmas)
+
+    for l in lemmas:
+        p = nltk.pos_tag([l])
+        if p[0][1] == 'NN':
+            posed.append(p[0][0])
+
+    #posed = [p[0] for p in nltk.pos_tag(lemmas) if p[1] == 'NN']
+    #print(posed)
+    c = Counter(posed)
+    #c = Counter([posed[0] for posed in nltk.pos_tag(lemmas) if posed[1] == 'NN'])
     print(*[i[1] for i in sorted([(i[1], i[0]) for i in c.most_common(50)], reverse=True)[:5]])
 
